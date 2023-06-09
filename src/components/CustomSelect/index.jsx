0493aec47f243627d5c3e8d1ded99f4a9fccf290
Hillel1408 <etemax@bk.ru>
2023-06-09 14:19:51 +0300

    FrontCloudCamp

---
+++ src/components/CustomSelect/index.jsx
@@ -0,0 +1,25 @@
+import Select from 'react-select';
+
+function CustomSelect(props) {
+    const customStyles = {
+        placeholder: (provided) => ({
+            ...provided,
+            color: 'rgba(0, 0, 0, 0.48)',
+            fontSize: '14px',
+            lineHeight: '20px',
+        }),
+        control: (provided) => ({
+            ...provided,
+            minHeight: '44px',
+        }),
+    };
+    return (
+        <Select
+            styles={customStyles}
+            options={props.options}
+            placeholder={props.placeholder}
+        />
+    );
+}
+
+export { CustomSelect };
--- src/components/Step1/index.jsx
+++ src/components/Step1/index.jsx
@@ -1,10 +1,22 @@
 import { useForm } from 'react-hook-form';
 import { Label } from '../Label';
 import { Button } from '../../components/Button';
+import { CustomSelect } from '../CustomSelect';
 import stylesInput from '../Input/Input.module.scss';
 import styles from './Step1.module.scss';
 
 function Step1({}) {
+    const options = [
+        {
+            value: 'man',
+            label: 'man',
+        },
+        {
+            value: 'woman',
+            label: 'woman',
+        },
+    ];
+
     const {
         register,
         handleSubmit,
@@ -12,6 +24,8 @@ function Step1({}) {
     } = useForm({
         defaultValues: {
             nickname: '',
+            name: '',
+            sername: '',
         },
         mode: 'onChange',
     });
@@ -22,22 +36,85 @@ function Step1({}) {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-            <Label>
-                Nickname
-                <input
-                    type="text"
-                    className={stylesInput.input}
-                    placeholder="Placeholder"
-                    {...register('nickname', {
-                        required: 'Поле обязательно к заполнению',
-                        maxLength: {
-                            value: 30,
-                            message: 'Максимум 30 символов',
-                        },
-                    })}
-                />
-            </Label>
-            <div>{errors?.nickname && errors?.nickname?.message}</div>
+            <div>
+                <Label>
+                    Nickname
+                    <input
+                        type="text"
+                        className={stylesInput.input}
+                        placeholder="Placeholder"
+                        id="field-nickname"
+                        {...register('nickname', {
+                            required: 'Поле обязательно к заполнению',
+                            maxLength: {
+                                value: 30,
+                                message: 'Максимум 30 символов',
+                            },
+                            pattern: {
+                                value: /^[A-zА-яЁё0-9]+$/,
+                                message: 'Только буквы и цифры',
+                            },
+                        })}
+                    />
+                </Label>
+                <p className="error">
+                    {errors?.nickname && errors?.nickname?.message}
+                </p>
+            </div>
+            <div>
+                <Label>
+                    Name
+                    <input
+                        type="text"
+                        className={stylesInput.input}
+                        placeholder="Placeholder"
+                        id="field-name"
+                        {...register('name', {
+                            required: 'Поле обязательно к заполнению',
+                            maxLength: {
+                                value: 50,
+                                message: 'Максимум 50 символов',
+                            },
+                            pattern: {
+                                value: /^[A-zА-яЁё]+$/,
+                                message: 'Только буквы',
+                            },
+                        })}
+                    />
+                </Label>
+                <p className="error">{errors?.name && errors?.name?.message}</p>
+            </div>
+            <div>
+                <Label>
+                    Sername
+                    <input
+                        type="text"
+                        className={stylesInput.input}
+                        placeholder="Placeholder"
+                        id="field-sername"
+                        {...register('sername', {
+                            required: 'Поле обязательно к заполнению',
+                            maxLength: {
+                                value: 50,
+                                message: 'Максимум 50 символов',
+                            },
+                            pattern: {
+                                value: /^[A-zА-яЁё]+$/,
+                                message: 'Только буквы',
+                            },
+                        })}
+                    />
+                </Label>
+                <p className="error">
+                    {errors?.sername && errors?.sername?.message}
+                </p>
+            </div>
+            <div>
+                <Label>
+                    Sex
+                    <CustomSelect options={options} placeholder="Не выбрано" />
+                </Label>
+            </div>
             <div className="buttons">
                 <Button
                     text="Назад"
