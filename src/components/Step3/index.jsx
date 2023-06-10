a47306d121c7e265ccef4b9d43f3bc193b41446f
Hillel1408 <etemax@bk.ru>
2023-06-10 12:20:21 +0300

    FrontCloudCamp

--- src/components/Step3/index.jsx
+++ src/components/Step3/index.jsx
@@ -1,6 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { Label } from '../Label';
+import { Button } from '../../components/Button';
 import styles from './Step3.module.scss';
+import stylesInput from '../Input/Input.module.scss';
+import classNames from 'classnames';
 
 function Step3({ active }) {
     const {
@@ -8,7 +12,9 @@ function Step3({ active }) {
         handleSubmit,
         formState: { errors },
     } = useForm({
-        defaultValues: {},
+        defaultValues: {
+            about: '',
+        },
         mode: 'onChange',
     });
 
@@ -22,7 +28,42 @@ function Step3({ active }) {
             className={styles.form}
             style={active === 3 ? { display: 'block' } : { display: 'none' }}
         >
-            123
+            <div>
+                <Label>
+                    About
+                    <textarea
+                        className={classNames(stylesInput.input, 'textarea')}
+                        placeholder="Placeholder"
+                        id="field-about"
+                        {...register('about', {
+                            required: 'Поле обязательно к заполнению',
+                            maxLength: {
+                                value: 200,
+                                message: 'Максимум 200 символов',
+                            },
+                        })}
+                    />
+                </Label>
+                <p className="error">
+                    {errors?.about && errors?.about?.message}
+                </p>
+            </div>
+            <div className="buttons">
+                <Button
+                    text="Назад"
+                    bg="white"
+                    color="#5558FA"
+                    id="button-back"
+                    href="/"
+                />
+                <Button
+                    text="Отправить"
+                    bg="#5558FA"
+                    color="white"
+                    id="button-send"
+                    type="submit"
+                />
+            </div>
         </form>
     );
 }
